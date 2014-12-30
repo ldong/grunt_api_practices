38fd1319838311de99f0c6eb92969394a4c41b89
Lin Dong <ldong@gatech.edu>
2014-12-30 15:59:39 -0500

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,4 +42,58 @@ module.exports = function(grunt){
         grunt.log.ok('last line');
     });
 
+    grunt.registerTask('files', function(){
+        var str = grunt.file.read('package.json');
+        grunt.log.writeln(str);
+
+        // json now is an JSON object
+        var json = grunt.file.readJSON('package.json');
+        console.log(json.devDependencies);
+
+        var yaml = grunt.file.readYAML('data_example.yaml');
+        console.log(yaml);
+
+        grunt.file.write('fileName.txt', 'some text');
+        grunt.file.copy('fileName.txt', 'other_copy.txt');
+        grunt.file.delete('other_copy.txt');
+
+        grunt.file.mkdir('newDir');
+        grunt.file.recurse('src', function(file){
+            grunt.log.ok(file);
+        });
+
+    });
+
+    grunt.registerTask('options', function(){
+        var target = grunt.option('target');
+        grunt.log.writeln(target);
+    });
+
+    grunt.registerTask('util', function(){
+        // what type of this object
+        grunt.log.writeln(grunt.util.kindOf([1,2,3]));
+
+        var norStr = grunt.util.normalizelf('This is a string\nOk?');
+        grunt.log.writeln(norStr);
+        var o = {
+            name: 'Lin',
+            obj: {
+                val1: 1,
+                val2: 2
+            },
+            arr: ['a', 'b', 'c']
+        };
+
+        grunt.util.recurse(o, function(val){
+            grunt.log.writeln(val);
+            // grunt.log.ok(val);
+        });
+
+        grunt.log.ok(grunt.util.repeat(3, 'Cool ') + 'Great!');
+
+        var num = 10;
+        grunt.log.ok(num+' '+grunt.util.pluralize(num, 'function/funtions'));
+
+        throw grunt.util.error('error');
+    });
 };
